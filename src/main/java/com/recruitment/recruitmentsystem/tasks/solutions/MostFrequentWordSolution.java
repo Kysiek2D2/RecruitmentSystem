074efd5b24b1fbadd5591d6e05a0d9def0266078
(KRDA) Krzysztof Dabkowski <krda@netcompany.com>
2026-02-26 13:40:45 +0100

    MostFrequentWord solution with simple for loops added

--- src/main/java/com/recruitment/recruitmentsystem/tasks/solutions/MostFrequentWordSolution.java
+++ src/main/java/com/recruitment/recruitmentsystem/tasks/solutions/MostFrequentWordSolution.java
@@ -1,5 +1,4 @@
 package com.recruitment.recruitmentsystem.tasks.solutions;
-import org.springframework.stereotype.Service;
 
 import java.util.*;
 
@@ -30,5 +29,38 @@ public class MostFrequentWordSolution {
 
         return topWord;
 
+    }
+
+    /**
+     * This is simple solution with for loops and no HashMap.
+     * It has worse performance than HashMap solution, but it's simple.
+     * @param text - strings separated by space
+     * @return
+     */
+    public static String mostFrequentWord2(String text) {
+        if(text == null || text.isEmpty()){
+            return "";
+        }
+
+        List<String> allWordsArray = Arrays.asList(text.toLowerCase().trim().split(" "));
+        Set<String> distinctWords = new HashSet<>(allWordsArray);
+
+        String mostCommonWord = null;
+        int mostCommonWordCount = 0;
+        for(String wordToSearch : distinctWords) {
+            int currentCount = 0;
+            for(String word : allWordsArray) {
+                if(wordToSearch.equals(word)) {
+                    currentCount ++;
+                }
+            }
+
+            if (currentCount > mostCommonWordCount) {
+                mostCommonWord = wordToSearch;
+                mostCommonWordCount = currentCount;
+            }
+        }
+
+        return mostCommonWord;
     }
 }
